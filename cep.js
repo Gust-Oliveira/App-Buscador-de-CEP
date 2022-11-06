b29db0e61b7b3b279a3b5e1afa8781d5c6ddd007
unknown <gustavo.oliveira201@hotmail.com>
2022-11-06 11:51:19 -0300

    funcionalidade de salvar cep

--- cep.js
+++ cep.js
@@ -30,3 +30,50 @@ function apagarCep(){
     document.getElementById("cidade").value = '';
     document.getElementById("uf").value = '';
 }
+function salvarCep(){
+    let cep = document.getElementById("cep").value;
+    let endereco = document.getElementById("endereco").value;
+    let bairro = document.getElementById("bairro").value;
+    let cidade = document.getElementById("cidade").value;
+    let uf = document.getElementById("uf").value;
+    
+    let divSalvar = document.createElement("div");
+    divSalvar.id = `${cep}`
+    divSalvar.className = 'divSalvar';
+
+    let p1 = document.createElement("div");
+    p1.innerHTML = `<strong>CEP</strong> : ${cep}`;
+    let p2 = document.createElement("div");
+    p2.innerHTML = `<strong>Endereço</strong> : ${endereco}`;
+    let p3 = document.createElement("div");
+    p3.innerHTML = `<strong>Bairro</strong> : ${bairro}`;
+    let p4 = document.createElement("div");
+    p4.innerHTML = `<strong>Cidade</strong> : ${cidade}`;
+    let p5 = document.createElement("div");
+    p5.innerHTML = `<strong>UF</strong> : ${uf}`;
+
+    let divApagar = document.createElement("div");
+    divApagar.id = `${cep}`;
+    divApagar.className = 'divApagar';
+    divApagar.innerHTML = `<strong>Apagar</strong>`;
+
+    divApagar.addEventListener("click", function(){
+        divSalvar.remove();
+    })
+    let hr = document.createElement("hr");
+
+    divSalvar.appendChild(p1)
+    divSalvar.appendChild(p2)
+    divSalvar.appendChild(p3)
+    divSalvar.appendChild(p4)
+    divSalvar.appendChild(p5)
+    divSalvar.appendChild(divApagar);
+    divSalvar.appendChild(hr);
+    document.getElementById("container-salvar").appendChild(divSalvar);
+
+    document.getElementById("cep").value = '';
+    document.getElementById("endereco").value = '';
+    document.getElementById("bairro").value = '';
+    document.getElementById("cidade").value = '';
+    document.getElementById("uf").value = '';
+}
